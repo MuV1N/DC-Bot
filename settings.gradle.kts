4ad228bcf2559be0b9948f8a7b7258ba0afc9a78
MuV1N <mussenvin3@gmail.com>
2023-10-07 15:48:46 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "DC-Bot"
+
---
+++ src/main/java/de/muv1n/Main.java
@@ -0,0 +1,7 @@
+package de.muv1n;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
